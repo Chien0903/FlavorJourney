60ca12782506fbfebafb8e87725c6442eae504c5
unknown <vuotngokhong@gmail.com>
2025-11-20 23:31:15 +0700

    fix error json

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -4,6 +4,8 @@ import viHomepage from "./locales/vi/homepage.json";
 import jpHomepage from "./locales/jp/homepage.json";
 import viSidebar from "./locales/vi/sidebar.json";
 import jpSidebar from "./locales/jp/sidebar.json";
+import viSearch from "./locales/vi/search.json";
+import jpSearch from "./locales/jp/search.json";
 
 const stored =
   typeof window !== "undefined" ? localStorage.getItem("lang") : null;
@@ -14,10 +16,10 @@ i18n.use(initReactI18next).init({
   fallbackLng: "vi",
   debug: false,
   defaultNS: "homepage",
-  ns: ["homepage", "sidebar"],
+  ns: ["homepage", "sidebar", "search"],
   resources: {
-    vi: { homepage: viHomepage, sidebar: viSidebar },
-    jp: { homepage: jpHomepage, sidebar: jpSidebar },
+    vi: { homepage: viHomepage, sidebar: viSidebar, search: viSearch },
+    jp: { homepage: jpHomepage, sidebar: jpSidebar, search: jpSearch },
   },
   interpolation: {
     escapeValue: false,
